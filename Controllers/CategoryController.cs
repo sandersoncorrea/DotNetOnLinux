c860f3a18c8e053fa43f944fa9d9417ee1e3b710
Sanderson Silva Corrêa <correasanderson@gmail.com>
2020-09-02 18:47:55 -0300

    get categories na controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testeef.Data;
+using testeef.Models;
+
+namespace testeef.Controllers
+{
+    [ApiController]
+    [Route("v1/categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Category>>> GetAction([FromServices] DataContext context)
+        {
+            var categories = context.Categories.ToListAsync();
+            return await categories;
+        }
+    }
+}
